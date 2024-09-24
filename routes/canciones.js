23631458de3d2b66483edf392e93504483a4153d
axelrust1 <130938390+axelrust1@users.noreply.github.com>
2024-09-24 20:45:22 -0300

    cambio

--- routes/canciones.js
+++ routes/canciones.js
@@ -4,5 +4,5 @@ const {getCanciones, getCancion} = require('../controllers/canciones')
 const rutas = Router()
 
 rutas.get('/', getCanciones) ///canciones?genero=pop ejemplo para buscar
-rutas.get('/:cantidad', getCancion) //canciones/10 para buscar 10 canciones del genero pop
+rutas.get('/:cantidad', getCancion) //canciones/10 para buscar 10 canciones del genero rkt
 module.exports = rutas
