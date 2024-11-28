9fd8c021827a77c9e24e6b98c3e4c3c94395b0a1
Agustin Santicchia <santicchiaagus85@gmail.com>
2024-11-28 20:23:55 -0300

    Correcciones

--- controllers/libros.js
+++ controllers/libros.js
@@ -2,10 +2,12 @@ const axios = require('axios');
 const { request, response } = require('express');
 
 const getLibros = (req = request, res = response) => {
-  const { tema = 'ficcion' } = req.query; 
+  const { tema = 'ficcion', page = 1, lang = 'en' } = req.query;
+  const maxResults = 50;
+  const startIndex = (page - 1) * maxResults;
 
   const apiKey = process.env.APIKEYLIBROS;
-  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:${tema}&key=${apiKey}&maxResults=40`;
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:${tema}&langRestrict=${lang}&startIndex=${startIndex}&maxResults=${maxResults}&key=${apiKey}`;
 
   if (!apiKey) {
     return res.status(500).json({
@@ -13,82 +15,75 @@ const getLibros = (req = request, res = response) => {
     });
   }
 
-  try {
-    axios.get(apiUrl)
-      .then((response) => {
-        const { items } = response.data;
-        res.status(200).json({
-          msg: `Libros más relevantes en el tema ${tema}`,
-          libros: items.map(item => ({
-            id: item.id,
-            titulo: item.volumeInfo.title,
-            autores: item.volumeInfo.authors,
-            descripcion: item.volumeInfo.description,
-            imagenPortada: item.volumeInfo.imageLinks?.thumbnail
-          })),
+  axios.get(apiUrl)
+    .then((response) => {
+      const { items } = response.data || {};
+      if (!items || items.length === 0) {
+        return res.status(404).json({
+          msg: 'No se encontraron libros con los filtros proporcionados',
         });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(400).json({
-          msg: 'Error al obtener los libros',
-          error,
-        });
-      });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      msg: 'Error interno del servidor',
-    });
-  }
-};
-
-const getLibro = (req = request, res = response) => {
-  const { cantidad = '10' } = req.params; 
-  const apiKey = process.env.APIKEYLIBROS;
-
-  if (!apiKey) {
-    return res.status(500).json({
-      msg: 'Error: API key no configurada',
-    });
-  }
-
-  try {
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:science+fiction&key=${apiKey}&maxResults=${cantidad}`;
-
-    axios.get(apiUrl)
-      .then((response) => {
-        const { items } = response.data;
-
-        const librosLimitados = items.map(item => ({
+      }
+      res.status(200).json({
+        msg: `Libros más relevantes en el tema "${tema}"`,
+        libros: items.map(item => ({
           id: item.id,
           titulo: item.volumeInfo.title,
           autores: item.volumeInfo.authors,
           descripcion: item.volumeInfo.description,
           imagenPortada: item.volumeInfo.imageLinks?.thumbnail
-        }));
-
-        res.status(200).json({
-          msg: `Los ${cantidad} libros más relevantes en el género de ciencia ficción`,
-          libros: librosLimitados,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(400).json({
-          msg: 'Error al obtener los libros',
-          error,
-        });
+        })),
       });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      msg: 'Error interno del servidor',
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).json({
+        msg: 'Error al obtener los libros',
+        error,
+      });
+    });
+};
+
+// Obtener un libro por Id
+const getLibroPorId = (req = request, res = response) => {
+  const { id } = req.params;
+  const apiKey = process.env.APIKEYLIBROS;
+
+  if (!apiKey) {
+    return res.status(500).json({
+      msg: 'Error: API key no configurada',
     });
   }
-}
+
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes/${id}?key=${apiKey}`;
+
+  axios.get(apiUrl)
+    .then((response) => {
+      const book = response.data;
+
+      if (!book) {
+        return res.status(404).json({
+          msg: 'No se encontró un libro con ese ID',
+        });
+      }
+
+      res.status(200).json({
+        id: book.id,
+        titulo: book.volumeInfo.title,
+        autores: book.volumeInfo.authors,
+        descripcion: book.volumeInfo.description,
+        imagenPortada: book.volumeInfo.imageLinks?.thumbnail
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).json({
+        msg: 'Error al obtener el libro',
+        error,
+      });
+    });
+};
 
 module.exports = {
   getLibros,
-  getLibro
-}
+  getLibroPorId
+};
